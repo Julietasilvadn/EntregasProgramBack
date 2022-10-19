bda752fba57286508a71a020abdd9fbda818ccec
Julieta Silva <juuli.siilva@gmail.com>
2022-10-19 12:08:02 -0300

    getAll() y save(falta ver hay un error ahi)

--- Clase4/main.js
+++ Clase4/main.js
@@ -7,22 +7,54 @@ class Contenedor {
 
     //Leer archivos
     async getAll(){
-        const info = await fs.promises.readFile(this.archivo, 'utf-8')
-        return info
-    }
+        try {
+            const info = await fs.promises.readFile(this.archivo, 'utf-8')
+            return JSON.parse(info)
+        } catch (error) {
+            console.log(error)
+        }
 
+    }
+    //Agregar archivos
     async save(producto){
-        const lista = await this.getAll()
-        const lista2= JSON.parse(lista)
-        const id = lista2[lista2.length-1].id+1
-        const nuevoProducto = {title:producto.title,
-                price:producto.price,
-                thumbnail:producto.thumbnail,
-                id:id}
-        lista2.push(nuevoProducto)
-        console.log(lista2)
-        await fs.promises.writeFile(this.archivo,lista2)
-        return id
+        try {
+            if( fs.existsSync(this.filename)){
+                const lista = await this.getAll()
+                if(lista.length>0){
+                    const id = lista[lista.length-1].id+1
+                    const nuevoProducto = {title:producto.title,
+                                           price:producto.price,
+                                           thumbnail:producto.thumbnail,
+                                           id:id}
+                    lista.push(nuevoProducto)
+                    await fs.promises.writeFile(this.archivo,JSON.stringify(lista, null, 2))
+                    return id
+                } else{ 
+                    const id = 1
+                    const nuevoProducto = {title:producto.title,
+                    price:producto.price,
+                    thumbnail:producto.thumbnail,
+                    id:id}
+                    lista.push(nuevoProducto)
+                    await fs.promises.writeFile(this.archivo,JSON.stringify(lista, null, 2))
+                    return id
+                }
+            } else {
+                const id = 1
+                const nuevoProducto = {title:producto.title,
+                    price:producto.price,
+                    thumbnail:producto.thumbnail,
+                    id:id}
+                
+                await fs.promises.writeFile(this.archivo,JSON.stringify([nuevoProducto], null, 2))
+                return id
+            }
+
+            
+        } catch (error) {
+            console.log(error)
+        }
+    
     }
 }
 
@@ -30,7 +62,9 @@ const contenedor = new Contenedor('productos.txt');
 
 async function mostrar(){
      console.log(await contenedor.getAll())
-     console.log(await contenedor.save({title: 'Escuadra',price: 123.45,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
+     console.log(await contenedor.save({title: 'Rectangulo',price: 153.65,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
+     console.log(await contenedor.getAll())
+     console.log(await contenedor.save({title: 'Escuadra', price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
      console.log(await contenedor.getAll())
 }
 
