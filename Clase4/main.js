92590bb96934ebd3b28fbf44f573d62a8fea2da1
Julieta Silva <juuli.siilva@gmail.com>
2022-10-20 14:00:22 -0300

    terminando

--- Clase4/main.js
+++ Clase4/main.js
@@ -20,33 +20,34 @@ class Contenedor {
         try {
             if(!fs.existsSync(this.archivo)){
                 
-                const nuevoProducto = {title:producto.title,
-                    price:producto.price,
-                    thumbnail:producto.thumbnail,
-                    id:1}
+                const nuevoProducto = await {title:producto.title,
+                                            price:producto.price,
+                                            thumbnail:producto.thumbnail,
+                                            id:1}
                 
                 await fs.promises.writeFile(this.archivo,JSON.stringify([nuevoProducto], null, 2))
                 return id
             } else {
                 const lista = await this.getAll()
                 if(lista.length>0){
-                    const id = lista[lista.length-1].id+1
-                    const nuevoProducto = {title:producto.title,
-                                           price:producto.price,
-                                           thumbnail:producto.thumbnail,
-                                           id:id}
-                    lista.push(nuevoProducto)
+                    const id = await lista[lista.length-1].id+1
+                    const nuevoProducto = await {title:producto.title,
+                                                price:producto.price,
+                                                thumbnail:producto.thumbnail,
+                                                 id:id}
+                    await lista.push(nuevoProducto)
                     await fs.promises.writeFile(this.archivo,JSON.stringify(lista, null, 2))
                     return id
                 } else{ 
-                    const id = 1
-                    const nuevoProducto = {title:producto.title,
-                                            price:producto.price,
-                                            thumbnail:producto.thumbnail,
-                                            id:id}
+                    const id = await 1
+                    const nuevoProducto = await {title:producto.title,
+                                                price:producto.price,
+                                                thumbnail:producto.thumbnail,
+                                                id:id}
                     lista.push(nuevoProducto)
                     await fs.promises.writeFile(this.archivo,JSON.stringify(lista, null, 2))
                     console.log(`ID ${id} agregada`)
+                    return id
                 }
             }
 
@@ -63,8 +64,8 @@ class Contenedor {
             const lista = await this.getAll()
             const listaVacia = []
             fs.promises.writeFile(this.archivo,JSON.stringify(listaVacia, null, 2))
-
-            console.log("Borrado Exitoso")
+        
+            return "Borrado Exitoso"
             
         } catch (error) {
             console.log(error)
@@ -76,13 +77,14 @@ class Contenedor {
         try {
             if (await fs.existsSync(this.archivo)) {
                 const data = await this.getAll();
-                const dataId = data.filter(producto => producto.id === id);
+                const dataId = await data.filter(producto => producto.id === id);
                 if (dataId.length === 0) {
                     throw new Error(
                         "No se encontro un producto con el id solicitado"
                     );
                 } else {
-                    console.log(`Producto con id ${id} encontrado:\n`, dataId);
+                    console.log( `Producto con id ${id} encontrado:`)
+                    return dataId
                 }
             }
         } catch (error) {
@@ -92,17 +94,12 @@ class Contenedor {
 
     async deleteById(id){
         try {
-            const encontrarId = await this.getById(id)
-            if(encontrarId != ''){
-                producto = await encontrarId.filter(producto => producto.id !== id)
-                await fs.writeFileSync('productos.txt',JSON.stringify(producto),'utf-8')
-            } else {
-                throw new Error (
-                    `No se encontro el producto con el id ${id}`
-                )
-            }
-            
-
+            if(await fs.existsSync(this.archivo)){
+                const data = await this.getAll();
+                const borrando = data.filter(producto => producto.id !== id);
+                await fs.promises.writeFile('productos.txt',JSON.stringify(borrando, null, 2))
+                return `ID ${id} borrada`
+            } 
         } catch (error) {
             console.log(error)
         }
@@ -115,6 +112,7 @@ const producto1 = {title: 'Rectangulo',price: 153.65,thumbnail: 'https://cdn3.ic
 const producto2 = {title: 'Escuadra', price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'};
 const producto3 = {title: 'Calculadora', price: 234.56, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'};
 const producto4 = {title: 'Globo Terráqueo', price: 345.67, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'};
+const producto5 = {title: 'PruebitaPastafrola', price: 222, thumbnail: 'file:///C:/Users/Jula/Downloads/pastafrola.jpg'};
 
 async function mostrar(){
      console.log(await contenedor.getAll())
@@ -122,10 +120,11 @@ async function mostrar(){
     //  console.log(await contenedor.save(producto2))
     //  console.log(await contenedor.save(producto3))
     //  console.log(await contenedor.save(producto4))
+    //  console.log(await contenedor.save(producto5))
     //  console.log(await contenedor.deleteAll())
     //  console.log(await contenedor.getAll())
-    console.log(await contenedor.getById(2))
-    console.log(await contenedor.getById(9))
+    // console.log(await contenedor.getById(2))
+    // console.log(await contenedor.getById(9))
     // console.log(await contenedor.deleteById(3))
     // console.log(await contenedor.getAll())
 }
