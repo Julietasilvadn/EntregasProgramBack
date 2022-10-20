f0f2b85a56b4a5e29f1bb117714e6174690c7d93
Julieta Silva <juuli.siilva@gmail.com>
2022-10-20 12:54:45 -0300

    agregue productos

--- Clase4/main.js
+++ Clase4/main.js
@@ -46,7 +46,7 @@ class Contenedor {
                                             id:id}
                     lista.push(nuevoProducto)
                     await fs.promises.writeFile(this.archivo,JSON.stringify(lista, null, 2))
-                    return id
+                    console.log(`ID ${id} agregada`)
                 }
             }
 
@@ -111,21 +111,24 @@ class Contenedor {
 }
 
 const contenedor = new Contenedor('productos.txt');
+const producto1 = {title: 'Rectangulo',price: 153.65,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'};
+const producto2 = {title: 'Escuadra', price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'};
+const producto3 = {title: 'Calculadora', price: 234.56, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'};
+const producto4 = {title: 'Globo Terráqueo', price: 345.67, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'};
 
 async function mostrar(){
      console.log(await contenedor.getAll())
-    //  console.log(await contenedor.save({title: 'Rectangulo',price: 153.65,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
-    //  console.log(await contenedor.getAll())
-    //  console.log(await contenedor.save({title: 'Escuadra', price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
-    //  console.log(await contenedor.getAll())
-    // console.log(await contenedor.getById(2))
+    //  console.log(await contenedor.save(producto1))
+    //  console.log(await contenedor.save(producto2))
+    //  console.log(await contenedor.save(producto3))
+    //  console.log(await contenedor.save(producto4))
     //  console.log(await contenedor.deleteAll())
     //  console.log(await contenedor.getAll())
     console.log(await contenedor.getById(2))
-    console.log(await contenedor.deleteById(3))
-    console.log(await contenedor.getAll())
+    console.log(await contenedor.getById(9))
+    // console.log(await contenedor.deleteById(3))
+    // console.log(await contenedor.getAll())
 }
 
 
 mostrar()
-
