dbe4ddd9634af24697488cd5c8098656eb7e85af
Julieta Silva <juuli.siilva@gmail.com>
2022-11-03 13:28:20 -0300

    ayuda tutor

--- Clase8/productos/productos.js
+++ Clase8/productos/productos.js
@@ -25,41 +25,33 @@ productos.save(producto1);
 productos.save(producto2);
 productos.save(producto3);
 
+
 const obtenerTodo=(peticion, respuesta) => {
     const listaProductos = productos.getAll();
     respuesta.json(listaProductos);
-}
+};
 
-//GET ALL******************************************
-rutaProductos.get('/', (peticion, respuesta) => {
-    const listaProductos = productos.getAll();
-    respuesta.json(listaProductos);
-  });
+const obternerPorId=(peticion, respuesta) => {
+    const id = parseInt(peticion.params.id);
+    const producto = productos.getById(id);
+    if (producto) {
+      respuesta.json(producto);
+    } else {
+      respuesta.status(404);
+      respuesta.json({ error : 'producto no encontrado' });
+    }
+    
+};
 
-//GET BY ID****************************************
-rutaProductos.get('/:id', (peticion, respuesta) => {
-  const id = parseInt(peticion.params.id);
-  const producto = productos.getById(id);
-  if (producto) {
-    respuesta.json(producto);
-  } else {
-    respuesta.status(404);
-    respuesta.json({ error : 'producto no encontrado' });
-  }
-  
-});
-
-//POST********************************************
-rutaProductos.post('/', (peticion, respuesta) => {
+const post=(peticion, respuesta) => {
     const producto = peticion.body;
     productos.save(producto);
     respuesta.json({
         status: "ok"
     });
-});
+};
 
-//PUT*********************************************
-rutaProductos.put('/:id', (peticion, respuesta) => {
+const putId=(peticion, respuesta) => {
     const producto = peticion.body;
     const id = parseInt(peticion.params.id);
     productos.deleteById(id);
@@ -68,18 +60,67 @@ rutaProductos.put('/:id', (peticion, respuesta) => {
     respuesta.json({
         status: "ok"
     });
-});
+};
 
-//DELETE BY ID************************************
-rutaProductos.delete('/:id', (peticion, respuesta) => {
+const deleteId=(peticion, respuesta) => {
     const producto = peticion.body;
     const id = parseInt(peticion.params.id);
     productos.deleteById(id);
     respuesta.json({
         status: "ok"
     })
-});
+};
+
+//GET ALL******************************************
+// rutaProductos.get('/', (peticion, respuesta) => {
+//     const listaProductos = productos.getAll();
+//     respuesta.json(listaProductos);
+//   });
+
+//GET BY ID****************************************
+// rutaProductos.get('/:id', (peticion, respuesta) => {
+//   const id = parseInt(peticion.params.id);
+//   const producto = productos.getById(id);
+//   if (producto) {
+//     respuesta.json(producto);
+//   } else {
+//     respuesta.status(404);
+//     respuesta.json({ error : 'producto no encontrado' });
+//   }
+  
+// });
+
+//POST********************************************
+// rutaProductos.post('/', (peticion, respuesta) => {
+//     const producto = peticion.body;
+//     productos.save(producto);
+//     respuesta.json({
+//         status: "ok"
+//     });
+// });
+
+//PUT*********************************************
+// rutaProductos.put('/:id', (peticion, respuesta) => {
+//     const producto = peticion.body;
+//     const id = parseInt(peticion.params.id);
+//     productos.deleteById(id);
+//     producto.id = id;
+//     productos.save({...producto, id});
+//     respuesta.json({
+//         status: "ok"
+//     });
+// });
+
+//DELETE BY ID************************************
+// rutaProductos.delete('/:id', (peticion, respuesta) => {
+//     const producto = peticion.body;
+//     const id = parseInt(peticion.params.id);
+//     productos.deleteById(id);
+//     respuesta.json({
+//         status: "ok"
+//     })
+// });
+//************************************************
 
 
-
-module.exports = {obtenerTodo};
+module.exports = {obtenerTodo, obternerPorId, post, putId, deleteId};
--- Clase8/rutas/routers.js
+++ Clase8/rutas/routers.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { Router } = express;
 const rutaProductos = Router();
+const {obtenerTodo,obternerPorId, post, putId, deleteId} = require("../productos/productos");
 
-const {obtenerTodo} = require("../productos/productos");
-
-rutaProductos.get('/', obtenerTodo)
+rutaProductos.get('/', obtenerTodo);
+rutaProductos.get('/:id', obternerPorId);
+rutaProductos.post('/', post);
+rutaProductos.put('/:id', putId);
+rutaProductos.delete('/:id', deleteId);
 
 
 
